9a0eb93b5cffeac7a4dbf0820ae64f3d88606604
Zorro1223 <151553673+Zorro1223@users.noreply.github.com>
2024-04-12 21:15:21 +0300

    Delete kb-backend-week-2-startkit-main directory

--- kb-backend-week-2-startkit-main/appModules/api/index.js
+++
@@ -1,9 +0,0 @@
-const { getData } = require("./api-utilis");
-const endpoints = require("./config");
-const getRandomGame = require("./api-utilis");
-
-module.exports = {
-    getData,
-    endpoints,
-    getRandomGame
-}
--- kb-backend-week-2-startkit-main/appModules/rating/rating-file.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs").promises;
-
-async function makeRatingFile(path, array){
-    
-}
