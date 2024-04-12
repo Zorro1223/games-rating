85a8a5f366eb60ea6ace0c9b8ac6f4c69a5a31a5
Zorro1223 <151553673+Zorro1223@users.noreply.github.com>
2024-04-12 21:15:56 +0300

    Add files via upload

---
+++ appModules/api/index.js
@@ -0,0 +1,9 @@
+const { getData } = require("./api-utilis");
+const endpoints = require("./config");
+const getRandomGame = require("./api-utilis");
+
+module.exports = {
+    getData,
+    endpoints,
+    getRandomGame
+}
